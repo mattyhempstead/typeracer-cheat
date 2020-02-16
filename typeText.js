404180ff540a07be14b64df5f79b7ccc2290b2b0
Matty <matty.hempstead@gmail.com>
2020-02-17 00:36:35 +1100

    Fix issue where loop does not end

--- typeText.js
+++ typeText.js
@@ -3,17 +3,20 @@ content = document.querySelector('.inputPanel span').parentElement.textContent.s
 charNum = 0;  // The index of the character we are up to
 
 typeNextCharacter = () => {
-  if (charNum == content.length - 1) return;
+  if (charNum == content.length) return true;
   input = document.querySelector('.txtInput');
   input.value += content[charNum++];
 
   eventObj = new Event("keydown");
   eventObj.keyCode = input.value.charCodeAt(0);
   input.dispatchEvent(eventObj);
+
 }
 
 loop = () => {
-  typeNextCharacter();
-  setTimeout(loop, 200 * Math.random());
+  const done = typeNextCharacter();
+  if (done) return;
+  setTimeout(loop, 24.5 + 2 * (Math.random() - 0.5));
 }
 loop();
+
